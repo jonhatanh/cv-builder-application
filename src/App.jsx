67b571c93ebc3e27bbd0980b621510f5b9c17686
jonhatanh <jonhatan.higuera@gmail.com>
2024-03-25 12:54:01 -0600

    CAdd constans for example data

--- src/App.jsx
+++ src/App.jsx
@@ -12,100 +12,15 @@ import {
   faGraduationCap,
   faMedal,
 } from "@fortawesome/free-solid-svg-icons";
+import { EDUCATION_EXAMPLE, EXPERIENCE_EXAMPLE, OTHERS_EXAMPLE, PERSONAL_DETAILS_EXAMPLE, SOCIAL_MEDIA_EXAMPLE } from "./constans";
 
 function App() {
-  const [personalDetails, setPersonalDetails] = useState({
-    name: "John Doe",
-    email: "john.doe@example.com",
-    phone: "123-456-7890",
-    skills:
-      "HTML, CSS, JavaScript, React, SASS, Tailwind, Node.js, PHP, Laravel, MySQL, Git",
-  });
-  const [socialMedia, setSocialMedia] = useState([
-    {
-      id: crypto.randomUUID(),
-      name: "LinkedIn",
-      link: "https://www.linkedin.com/in/john-doe/",
-      iconName: "LinkedIn",
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "GitHub",
-      link: "https://github.com/johndoe",
-      iconName: "GitHub",
-    },
-  ]);
-  const [education, setEducation] = useState([
-    {
-      id: crypto.randomUUID(),
-      title: "UNIVERSITY OF EXAMPLE",
-      subtitle: "Degree in Computer Science",
-      date: "09/2018 - 06/2022",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      bullets: [
-        {
-          id: crypto.randomUUID(),
-          text: "GPA: 3.8 / 4.0",
-        },
-        {
-          id: crypto.randomUUID(),
-          text: "Graduation Date: 06/2022",
-        },
-      ],
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "EXAMPLE TECHNICAL INSTITUTE",
-      subtitle: "Degree in Software Development",
-      date: "09/2014 - 06/2018",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      bullets: [
-        {
-          id: crypto.randomUUID(),
-          text: "GPA: 3.6 / 4.0",
-        },
-      ],
-    },
-  ]);
+  const [personalDetails, setPersonalDetails] = useState(PERSONAL_DETAILS_EXAMPLE);
+  const [socialMedia, setSocialMedia] = useState(SOCIAL_MEDIA_EXAMPLE);
+  const [education, setEducation] = useState(EDUCATION_EXAMPLE);
 
-  const [experience, setExperience] = useState([
-    {
-      id: crypto.randomUUID(),
-      title: "EXAMPLE COMPANY",
-      subtitle: "Software Engineer",
-      date: "07/2022 - Present",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      bullets: [
-        {
-          id: crypto.randomUUID(),
-          text: "Developed and maintained web applications using React and Node.js.",
-        },
-        {
-          id: crypto.randomUUID(),
-          text: "Collaborated with cross-functional teams to deliver high-quality software solutions.",
-        },
-        {
-          id: crypto.randomUUID(),
-          text: "Utilized Git for version control and participated in code reviews.",
-        },
-      ],
-    },
-  ]);
-  const [others, setOthers] = useState([
-    {
-      id: crypto.randomUUID(),
-      title: "EXAMPLE BOOTCAMP",
-      subtitle: "Full Stack Web Development Bootcamp",
-      date: "01/2023 - 06/2023",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      bullets: [
-        {
-          id: crypto.randomUUID(),
-          text: "Completed an intensive bootcamp covering both frontend and backend technologies.",
-        },
-      ],
-    },
-  ]);
+  const [experience, setExperience] = useState(EXPERIENCE_EXAMPLE);
+  const [others, setOthers] = useState(OTHERS_EXAMPLE);
 
   //Personal Details Form
   function handlePersonalDetails(property, value) {
---
+++ src/constans.js
@@ -0,0 +1,93 @@
+export const PERSONAL_DETAILS_EXAMPLE = {
+  name: "John Doe",
+  email: "john.doe@example.com",
+  phone: "123-456-7890",
+  skills:
+    "HTML, CSS, JavaScript, React, SASS, Tailwind, Node.js, PHP, Laravel, MySQL, Git",
+};
+export const SOCIAL_MEDIA_EXAMPLE = [
+  {
+    id: crypto.randomUUID(),
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/john-doe/",
+    iconName: "LinkedIn",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "GitHub",
+    link: "https://github.com/johndoe",
+    iconName: "GitHub",
+  },
+];
+export const EDUCATION_EXAMPLE = [
+  {
+    id: crypto.randomUUID(),
+    title: "UNIVERSITY OF EXAMPLE",
+    subtitle: "Degree in Computer Science",
+    date: "09/2018 - 06/2022",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    bullets: [
+      {
+        id: crypto.randomUUID(),
+        text: "GPA: 3.8 / 4.0",
+      },
+      {
+        id: crypto.randomUUID(),
+        text: "Graduation Date: 06/2022",
+      },
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "EXAMPLE TECHNICAL INSTITUTE",
+    subtitle: "Degree in Software Development",
+    date: "09/2014 - 06/2018",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    bullets: [
+      {
+        id: crypto.randomUUID(),
+        text: "GPA: 3.6 / 4.0",
+      },
+    ],
+  },
+];
+
+export const EXPERIENCE_EXAMPLE = [
+  {
+    id: crypto.randomUUID(),
+    title: "EXAMPLE COMPANY",
+    subtitle: "Software Engineer",
+    date: "07/2022 - Present",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    bullets: [
+      {
+        id: crypto.randomUUID(),
+        text: "Developed and maintained web applications using React and Node.js.",
+      },
+      {
+        id: crypto.randomUUID(),
+        text: "Collaborated with cross-functional teams to deliver high-quality software solutions.",
+      },
+      {
+        id: crypto.randomUUID(),
+        text: "Utilized Git for version control and participated in code reviews.",
+      },
+    ],
+  },
+];
+
+export const OTHERS_EXAMPLE = [
+  {
+    id: crypto.randomUUID(),
+    title: "EXAMPLE BOOTCAMP",
+    subtitle: "Full Stack Web Development Bootcamp",
+    date: "01/2023 - 06/2023",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    bullets: [
+      {
+        id: crypto.randomUUID(),
+        text: "Completed an intensive bootcamp covering both frontend and backend technologies.",
+      },
+    ],
+  },
+];
